46cdeab085ac2eed214ac7308bd053045eb1963e
Aaliyah <74381305+Aaliyahmp@users.noreply.github.com>
2021-01-14 16:08:44 -0600

    Add files via upload

---
+++ Calc2/src/com/tts/Calculator.java
@@ -0,0 +1,56 @@
+package com.tts;
+
+import java.util.Scanner;
+
+public class Calculator<input> {
+
+    //
+//    public int add(int num1, int num2) {
+//        return num1 + num2;
+//    }
+//
+//    public int sub(int num1, int num2) {
+//        return num1 - num2;
+//    }
+//
+//    public int div(int num1, int num2) {
+//        return num1 / num2;
+//    }
+//
+//    public int mul(int num1, int num2) {
+//        return num1 * num2;
+//    }
+//
+    public static int square(int num1) {
+        return num1 * num1;
+
+
+    }
+}
+//    public static void square() {
+//        Scanner square = new Scanner(System.in);
+//        System.out.println("Enter a number:");
+//        int input = Integer.parseInt(square.nextLine());
+//int operation = Calculator.square(input);
+//        System.out.println("The square root is: " + operation);
+//    }
+//}
+
+
+//
+//    System.out.println("Enter a number:")
+//    private Scanner factoral;
+//    int input = factoral.nextInt();
+//    int i, fact = 1;
+//    int number = input;//It is the number to calculate factorial
+//  for(i=1;i<=number;i++)
+//
+//    {
+//        fact = fact * i;
+//    }
+//
+//
+//System.out.println("Factorial of "+number+" is: "+fact);
+//
+//}
+
---
+++ Calc2/src/com/tts/Main.java
@@ -0,0 +1,140 @@
+package com.tts;
+
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner square = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int input = Integer.parseInt(square.nextLine());
+        int operation = Calculator.square(input);
+        System.out.println("The square root is: " + operation);
+
+//factorial
+        Scanner factoral = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int input2 = Integer.parseInt(factoral.nextLine());
+
+        int i, fact = 1;
+        int number = input2;//It is the number to calculate factorial
+        for (i = 1; i <= number; i++) {
+            fact = fact * i;
+        }
+
+
+        System.out.println("Factorial of " + number + " is: " + fact);
+//Sin, cos, tan
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Radain or degree mode?: ");
+        String radainOrDegree = in.nextLine();
+
+        if(radainOrDegree.equalsIgnoreCase("degree")){
+            System.out.print("Which operation? (sin, cos, tan):");
+            String operation2 = in.nextLine();
+            switch(operation2){
+                case "sin":
+                    System.out.print("What number do you want in the sin: ");
+                    System.out.println(Math.sin(Math.toRadians(in.nextInt())));
+                    break;
+                case "cos":
+                    System.out.print("What number do you want in the cos: ");
+                    System.out.println(Math.cos(Math.toRadians(in.nextInt())));
+                    break;
+                case "tan":
+                    System.out.print("What number do you want in the tan: ");
+                    System.out.println(Math.tan(Math.toRadians(in.nextInt())));
+                    break;
+                default:
+                    break;
+            }
+        } else{
+            System.out.print("Which operation? (sin, cos, tan):");
+            String operation2 = in.nextLine();
+            switch(operation2){
+                case "sin":
+                    System.out.print("What number do you want in the sin: ");
+                    System.out.println(Math.sin(in.nextInt()));
+                    break;
+                case "cos":
+                    System.out.print("What number do you want in the cos: ");
+                    System.out.println(Math.cos(in.nextInt()));
+                    break;
+                case "tan":
+                    System.out.print("What number do you want in the tan: ");
+                    System.out.println(Math.tan(in.nextInt()));
+                    break;
+                default:
+                    break;
+            }
+        }
+        in.close();
+
+        //other operations
+            Scanner NumInput = new Scanner(System.in);
+            double firstNum = 0;
+            double secondNum = 0;
+            double result = 0;
+            System.out.println("Enter first number: ");
+            firstNum = NumInput.nextDouble();
+            System.out.println("Enter operator: ");
+            String amalgar = NumInput.next();
+            if (amalgar == "sin" || amalgar == "cos" || amalgar == "tan" || amalgar == "cot") {
+                switch (amalgar) {
+                    case "sin":
+                        result = Math.toRadians(Math.sin(firstNum));
+                        break;
+                    case "cos":
+                        result = Math.toRadians(Math.cos(firstNum));
+                        break;
+                    case "tan":
+                        result = Math.toRadians(Math.tan(firstNum));
+                        break;
+                    case "cot":
+                        result = (Math.toRadians(Math.cos(firstNum)) / Math.toRadians(Math.sin(firstNum)));
+                        break;
+
+                    default:
+                        break;
+
+
+                }
+                System.out.println(Math.toRadians(result));
+            } else
+
+                System.out.println("Enter second number: ");
+            secondNum = NumInput.nextDouble();
+            switch (amalgar) {
+                case "+":
+                    ;
+                    result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+                case "*":
+                    result = firstNum * secondNum;
+                    break;
+                case "/":
+                    result = firstNum / secondNum;
+                    break;
+
+                default:
+                    System.out.println("nemifahmam chi neveeshti");
+            }
+
+            System.out.println(result);
+
+    }
+}
+
+
+
+
+
